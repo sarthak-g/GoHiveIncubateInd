ad6830d9726ab941bd8fbcfc7412aad4a535f214
sarthak-g <sarthakgupta041@gmail.com>
2019-06-02 10:46:36 +0530

    code updated

--- evapp/urls.py
+++ evapp/urls.py
@@ -4,4 +4,6 @@ urlpatterns = [
     path('', include('django.contrib.auth.urls')),
     path('customer/', include('django.contrib.auth.urls')),
     path('signup/',views.SignUpView,name='signup'),
+    path('customerpanel/',views.CustomerView,name='customer'),
+    path('adminpanel/',views.AdminView,name='admin'),
 ]
